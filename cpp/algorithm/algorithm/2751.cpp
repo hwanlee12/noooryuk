4328640eb19d1bf0675a847eab69efae043ec1a2
hwanlee12 <tmdghks912@naver.com>
2022-09-03 23:00:18 +0900

    2751

--- cpp/algorithm/algorithm/2751.cpp
+++ cpp/algorithm/algorithm/2751.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
+    cin.tie(0);
+    cout.tie(0);
 
+    int n;
+    cin >> n;
 
+    vector<int> v;
+    for(int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < n; i++){
+        cout << v[i] << '\n';
+    }
 
     return 0;
 }
