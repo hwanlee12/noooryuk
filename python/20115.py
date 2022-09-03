691fed98562e7231995fc1e84f4a386dc92038c4
hwanlee12 <tmdghks912@naver.com>
2022-09-03 22:42:30 +0900

    all reset

--- python/20115.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-n = float(input())
-drink = list(map(float, input().split()))
-drink.sort()
-ans = drink[-1]
-for item in drink[:int(n) - 1]:
-    ans += item / 2
-print(ans)
